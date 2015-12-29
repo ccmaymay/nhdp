fafc8d2b7c5bb977ce0ff899f38c46c72d3693d5
Chandler May <cjmay4754@gmail.com>
2015-12-29 15:50:56 -0500

    fix remaining bugs

--- parse_doc.m
+++ parse_doc.m
@@ -21,12 +21,12 @@ spaces = spaces(spaces < hash);
 colons = findstr(':', s);
 
 num_types = length(spaces);
-doc_ids = zeros(1,num_types);
+doc_ids = zeros(1,num_types,'uint64');
 doc_counts = zeros(1,num_types);
 
 start = 1;
 for i=1:num_types
-    doc_ids(i) = str2num(s(start:(colons(i)-1)));
+    doc_ids(i) = str2num(s(start:(colons(i)-1))) + 1;
     doc_counts(i) = str2num(s((colons(i)+1):(spaces(i)-1)));
     start = spaces(i) + 1;
 end
--- script_nHDP.m
+++ script_nHDP.m
@@ -1,50 +1,116 @@
 % initialize / use a large subset of documents (e.g., 10,000) contained in Xid and Xcnt to initialize
 %num_topics = [20 10 5];
-name = 'giga-deps'
-num_topics = [9 7 5]
+name = 'giga-deps';
+num_topics = [9 7 5];
+iota = 64;
+kappa = 0.6;
+beta0 = .1; % this parameter is the Dirichlet base distribution and can be played with
+batch_size = 2000;
+init_rand_frac = 1/20;
+max_init_batch_size = 10000;
+
+addpath /scratch/groups/bvandur1/redis-mat
+input_host = 'compute0054';
+input_port = 61236;
+input_db = redis(input_host, input_port);
+data_key = 'giga:pll:agg:10k/bow';
+vocab_key = 'giga:pll:agg:10k/vocab';
+
 level_num_topics = 1;
 total_num_topics = 0;
 for i=1:length(num_topics)
     level_num_topics = level_num_topics * num_topics(i);
-    total_num_topics += level_num_topics;
+    total_num_topics = total_num_topics + level_num_topics;
 end
-total_num_topics
-batch_size = 2000
-addpath ../redis-matlab/src
-input_host = 'compute0054'
-input_port = 61236
-input_db = redisConnection(input_host, input_port);
-data_key = 'giga:pll:agg:10k/bow'
-vocab_key = 'giga:pll:agg:10k/vocab'
-D = redisScard(input_db, data_key)
-W = redisLLen(input_db, vocab_key)
-doc_scale = 100
-scale = D * doc_scale / total_num_topics
+
+reply = command(input_db, 'scard %s', data_key);
+if reply.type ~= redisReplyType.INTEGER || reply.data == 0
+    if reply.type == redisReplyType.ERROR
+        disp(reply.data)
+    else
+        disp(reply.type)
+    end
+    error('unexpected reply to dataset scard')
+end
+D = reply.data;
+
+reply = command(input_db, 'llen %s', vocab_key);
+if reply.type ~= redisReplyType.INTEGER || reply.data == 0
+    if reply.type == redisReplyType.ERROR
+        disp(reply.data)
+    else
+        disp(reply.type)
+    end
+    error('unexpected reply to vocab llen')
+end
+W = reply.data;
+
+init_batch_size = min(D, max_init_batch_size);
+
+disp(sprintf('loading %d initialization documents...', init_batch_size));
+reply = command(input_db, 'srandmember %s %d', data_key, init_batch_size);
+if reply.type ~= redisReplyType.ARRAY
+    if reply.type == redisReplyType.ERROR
+        disp(reply.data)
+    else
+        disp(reply.type)
+    end
+    error('unexpected reply to dataset srandmember')
+end
+[Xid, Xcnt, status] = parse_redis_docs(reply);
+if ~status
+    error('document parse error')
+end
+
+%doc_scale = 100;
+doc_scale = mean(cellfun(@sum, Xcnt));
+
+disp('initializing...')
+scale = D * doc_scale / total_num_topics;
+tic
 Tree = nHDP_init(Xid,Xcnt,num_topics,scale);
+toc
 for i = 1:length(Tree)
     if Tree(i).cnt == 0
         Tree(i).beta_cnt(:) = 0;
     end
     vec = gamrnd(ones(1,length(Tree(i).beta_cnt)),1);
-    Tree(i).beta_cnt = .95*Tree(i).beta_cnt + .05*scale*vec/sum(vec);
+    Tree(i).beta_cnt = (1 - init_rand_frac)*Tree(i).beta_cnt + init_rand_frac*scale*vec/sum(vec);
 end
 
 % main loop / to modify this, at each iteration send in a new subset of docs
 % contained in Xid_batch and Xcnt_batch
-beta0 = .1 % this parameter is the Dirichlet base distribution and can be played with
 i = 1;
 while true
     disp(['iteration ' num2str(i)])
-    if isinteger(log2(i))
+    log2i = log2(i);
+    if ceil(log2i) == log2i
         path = sprintf('%s-%d.mat', name, i);
         disp(['saving to ' path])
-        save(path, '-v7.3', name, num_topics, scale, beta0, i, Tree)
+        save(path, '-v7.3', 'name', 'num_topics', 'scale', 'beta0', 'batch_size', 'i', 'Tree');
     end
+
     %[a,b] = sort(rand(1,length(Xid)));
-    rho = (64+i)^-.6; % step size can also be played with
-    redisSRandmember(input_db, data_key, batch_size);
     %Xid_batch = Xid(b(1:2000));
     %Xcnt_batch = Xcnt(b(1:2000));
+
+    reply = command(input_db, 'srandmember %s %d', data_key, batch_size);
+    if reply.type ~= redisReplyType.ARRAY
+        if reply.type == redisReplyType.ERROR
+            disp(reply.data)
+        else
+            disp(reply.type)
+        end
+        error('unexpected reply to dataset srandmember')
+    end
+    [Xid_batch, Xcnt_batch, status] = parse_redis_docs(reply);
+    if ~status
+        error('document parse error')
+    end
+
+    tic
+    rho = (iota+i)^-kappa; % step size can also be played with
     Tree = nHDP_step(Xid_batch,Xcnt_batch,Tree,scale,rho,beta0);
     i = i + 1;
+    toc
 end
