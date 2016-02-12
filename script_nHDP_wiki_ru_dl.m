3511f2ce9b66af7b583b083a6afb8ea6e205437d
Chandler May <cjmay4754@gmail.com>
2016-02-12 16:46:00 -0500

    wiki ru scripts

---
+++ script_nHDP_wiki_ru_dl.m
@@ -0,0 +1,6 @@
+name = 'wiki-ru-dl'
+input_host = 'r7n14';
+input_port = 61238;
+data_key = 'wiki.ru:dl:nz/bow';
+vocab_key = 'wiki.ru:dl/vocab';
+run_nHDP(name, input_host, input_port, data_key, vocab_key);
---
+++ script_nHDP_wiki_ru_dll.m
@@ -0,0 +1,6 @@
+name = 'wiki-ru-dll'
+input_host = 'r7n14';
+input_port = 61238;
+data_key = 'wiki.ru:dll:nz/bow';
+vocab_key = 'wiki.ru:dll/vocab';
+run_nHDP(name, input_host, input_port, data_key, vocab_key);
