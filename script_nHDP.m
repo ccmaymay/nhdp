c7e17d8b4ca88993064a7bd35588beb900d09428
Chandler May <chandler.camille.may@gmail.com>
2021-05-21 18:50:22 -0400

    Define constants in script_nHDP.

--- script_nHDP.m
+++ script_nHDP.m
@@ -1,6 +1,8 @@
 % initialize / use a large subset of documents (e.g., 10,000) contained in Xid and Xcnt to initialize
 num_topics = [20 10 5];
 scale = 100000;
+batch_size = 2000;
+num_iters = 1000;
 Tree = nHDP_init(Xid,Xcnt,num_topics,scale);
 for i = 1:length(Tree)
     if Tree(i).cnt == 0
@@ -13,10 +15,10 @@ end
 % main loop / to modify this, at each iteration send in a new subset of docs
 % contained in Xid_batch and Xcnt_batch
 beta0 = .1; % this parameter is the Dirichlet base distribution and can be played with
-for i = 1:1000
+for i = 1:num_iters
     [a,b] = sort(rand(1,length(Xid)));
     rho = (1+i)^-.75; % step size can also be played with
-    Xid_batch = Xid(b(1:2000));
-    Xcnt_batch = Xcnt(b(1:2000));
+    Xid_batch = Xid(b(1:batch_size));
+    Xcnt_batch = Xcnt(b(1:batch_size));
     Tree = nHDP_step(Xid_batch,Xcnt_batch,Tree,scale,rho,beta0);
 end
