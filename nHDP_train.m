34ae5ff0aa903e4ade204cf52081890176986156
Chandler May <chandler.camille.may@gmail.com>
2021-05-24 09:24:27 -0400

    Add ability to resume training.

--- nHDP_train.m
+++ nHDP_train.m
@@ -4,9 +4,11 @@ arguments
     X (:,:) {mustBeInteger,mustBeNonnegative}
     options.num_topics (1,:) {mustBeInteger} = [20 10 5]
     options.scale (1,1) {mustBePositive} = 100000
-    options.init_size (1,1) {mustBeInteger} = 2000
-    options.batch_size (1,1) {mustBeInteger} = 2000
-    options.num_iters (1,1) {mustBeInteger} = 1000
+    options.init_size (1,1) {mustBeInteger,mustBeNonnegative} = 2000
+    options.batch_size (1,1) {mustBeInteger,mustBePositive} = 2000
+    options.num_iters (1,1) {mustBeInteger,mustBePositive} = 1000
+    options.start_iter (1,1) {mustBeInteger,mustBePositive} = 1
+    options.start_tree (1,:) = []
     options.beta0 (1,1) {mustBePositive} = 0.1
     options.rho_exp (1,1) {mustBePositive} = 0.75
     options.save_interval (1,1) {mustBeInteger,mustBeNonnegative} = 0
@@ -19,20 +21,27 @@ options.batch_size = min(size(X, 1), options.batch_size);
 [D, Voc] = size(X);
 fprintf('corpus has %d documents spanning %d words\n', D, Voc);
 
-disp('initializing topics with k-means algorithm...');
-[~,b] = sort(rand(1,D));
-Tree = nHDP_init(X(b(1:options.init_size),:),options.num_topics,options.scale);
-disp('post-processing initialized topics...');
-for i = 1:length(Tree)
-    if Tree(i).cnt == 0
-        Tree(i).beta_cnt(:) = 0;
+if options.init_size > 0
+    disp('initializing topics with k-means algorithm...');
+    [~,b] = sort(rand(1,D));
+    Tree = nHDP_init(X(b(1:options.init_size),:),options.num_topics,options.scale);
+    disp('post-processing initialized topics...');
+    for i = 1:length(Tree)
+        if Tree(i).cnt == 0
+            Tree(i).beta_cnt(:) = 0;
+        end
+        vec = gamrnd(ones(1,length(Tree(i).beta_cnt)),1);
+        Tree(i).beta_cnt = .95*Tree(i).beta_cnt + .05*options.scale*vec/sum(vec);
     end
-    vec = gamrnd(ones(1,length(Tree(i).beta_cnt)),1);
-    Tree(i).beta_cnt = .95*Tree(i).beta_cnt + .05*options.scale*vec/sum(vec);
+else
+    if isempty(options.start_tree)
+        error('init_size is 0 but start_tree is not specified');
+    end
+    Tree = options.start_tree;
 end
 
 disp('estimating topics with variational inference...');
-for i = 1:options.num_iters
+for i = options.start_iter:options.num_iters
     fprintf('beginning variational inference iteration %d/%d...\n', i, options.num_iters);
     [~,b] = sort(rand(1,D));
     rho = (1+i)^-options.rho_exp; % step size can also be played with
